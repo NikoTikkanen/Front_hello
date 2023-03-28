5a4d777848d851d7911230eba4b3974aa7e81165
NikoTikkanen <123297498+NikoTikkanen@users.noreply.github.com>
2023-03-28 18:33:58 +0300

    Add files via upload
    mui tabs

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,15 +1,15 @@
-import React, {useState } from 'react'
-import {useRef} from 'react';
+import { Stack } from '@mui/material';
+import Button from '@mui/material/Button';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import TextField from '@mui/material/TextField';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+import { AgGridReact } from 'ag-grid-react';
+import React, { useRef, useState } from 'react';
 
-import { AgGridReact } from 'ag-grid-react'
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-material.css'
-
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs' 
 
 
 
@@ -48,11 +48,33 @@ const columns =[
   {field: "description", sortable: true, filter: true},
   {field: "date", sortable: true, filter: true},
   {field: "priority", sortable: true, filter: true,
-    cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}}
+  cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}}
 ]
+
+const [value, setValue] = useState('Home');
+
+const handleChange = (event, value) => {
+  setValue(value);
+}
+
 return (
+  
   <div>
+    <Tabs value={value} onChange={handleChange}>
+      <Tab value="Home" label="Home" />
+
+      <Tab value="Todo" label="Todolist" />
+      
+    </Tabs>
+
+    {value === 'Home' && <div>
+      <h1>Home</h1>
+    <div>Tervetuloa
+      </div></div>}
+      {value === 'Todo' && <div><h1>Todo</h1>
     
+     
+    <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
     <TextField
       label="Description"
       placeholder="Description"
@@ -78,8 +100,8 @@ return (
                         value={todo.date}
                         //onChange={date => setTodo({ ...todo, date})}
                         onChange={pickDate}
-
-                    />}
+                        
+                        />}
 </LocalizationProvider>      
 
     <TextField
@@ -89,11 +111,14 @@ return (
       value={todo.priority} 
       //onChange={e => setTodo({ ...todo, priority: e.target.value })}
       onChange={inputChanged}
-
+      
       />
 
     <Button onClick={addTodo} variant="contained">Add Todo</Button>
     <Button onClick={deleteTodo} variant="outlined">Delete</Button>
+
+    </Stack>
+
 
   <div className="ag-theme-material"
   style={{height:'700px', width: '70%', margin: 'auto'}}>
@@ -108,9 +133,16 @@ return (
       </AgGridReact>
   </div>
 
+        </div>}
  
   </div>
 );
 };
+export default Todolist
 
-export default Todolist;
+    
+
+
+
+
+
